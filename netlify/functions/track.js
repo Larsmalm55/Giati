39952da62739294056449a55a10c40ea84439b82
Larsmalm55 <larsmalmedal55@gmail.com>
2026-05-20 20:26:52 +0200

    Refactor rSet function to simplify fetch call

--- netlify/functions/track.js
+++ netlify/functions/track.js
@@ -1,7 +1,5 @@
 async function rSet(key, value) {
-  await fetch(`${REDIS_URL}/set/${encodeURIComponent(key)}`, {
-    method: 'POST',
-    headers: { Authorization: `Bearer ${REDIS_TOKEN}`, 'Content-Type': 'application/json' },
-    body: JSON.stringify(JSON.stringify(value))
+  await fetch(`${REDIS_URL}/set/${encodeURIComponent(key)}/${encodeURIComponent(JSON.stringify(value))}`, {
+    headers: { Authorization: `Bearer ${REDIS_TOKEN}` }
   });
 }
